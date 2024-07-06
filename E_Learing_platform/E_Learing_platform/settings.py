642b7a8626414418224bce72cbfdc798a052e48b
mr-aniket-2004 <157163406+mr-aniket-2004@users.noreply.github.com>
2024-07-06 14:02:50 +0530

    frist

--- E_Learing_platform/E_Learing_platform/settings.py
+++ E_Learing_platform/E_Learing_platform/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'E_Learing_platform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# add manually 
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
