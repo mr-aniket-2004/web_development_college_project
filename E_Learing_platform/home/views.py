be7a6b0b6b3d19b0a572dde8e1ff6b0e3644a89b
mr-aniket-2004 <157163406+mr-aniket-2004@users.noreply.github.com>
2024-07-09 00:25:39 +0530

    TRY

--- E_Learing_platform/home/views.py
+++ E_Learing_platform/home/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html")
 
-def about(request):
-    return render(request, "about.html")
+def course(request):
+    return render(request, "course.html")
