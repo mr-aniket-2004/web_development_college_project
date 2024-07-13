586c46f74f1c2744462cb4d48255b4d145900aee
mr-aniket-2004 <157163406+mr-aniket-2004@users.noreply.github.com>
2024-07-13 20:39:15 +0530

    new

--- E_Learing_platform/home/urls.py
+++ E_Learing_platform/home/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("",views.index, name= "home"),
     path("about/",views.course, name= "about"),
     path("home/",views.index, name="home"),
-    path("about/python/",views.python,name="python"),
-    path("about/java/",views.java,name="java")
+    path("python/",views.python,name="python"),
+    path("java/",views.java,name="java")
 ]
