0f35e5a7b425ea606b09476e047e168b587865ee
mr-aniket-2004 <aniketchandiwadeprofessional@gmail.com>
2024-07-18 11:11:07 +0530

    new

--- E_Learing_platform/home/views.py
+++ E_Learing_platform/home/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import *
 from datetime import datetime
 from home.models import Contact
 from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
@@ -23,7 +24,10 @@ def contactus(request):
         contact = Contact(fname=fname,sname=sname,email=email,phoneno=phoneno,desc=desc,date=datetime.today())
         contact.save()
         messages.success(request, "Your form submit sucessfully!")
+    # return redirect('contact-us')
     return render(request, "contact-us.html")
+
+
 
 def log(request):
     return render(request, "log-sign.html")
